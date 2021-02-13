e468cc7994ef587395dbb20ce748018af5b89d29
Rushad Mehta <rushadmehta16@gmail.com>
2021-02-13 17:30:48 +0530

    Added check for empty username signups. Optimised imports. Removed redundant methods.

--- app/src/main/java/com/example/chatroom/SignUp.kt
+++ app/src/main/java/com/example/chatroom/SignUp.kt
@@ -5,10 +5,6 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.gson.Gson
-import io.ktor.client.*
-import io.ktor.client.engine.android.*
-import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -20,7 +16,6 @@ import kotlin.coroutines.CoroutineContext
 class SignUp : AppCompatActivity(), CoroutineScope {
 
     //START COROUTINE
-    private val gson = Gson()
     private var job: Job = Job()
     override val coroutineContext: CoroutineContext
         get() = Dispatchers.Main + job
@@ -36,44 +31,39 @@ class SignUp : AppCompatActivity(), CoroutineScope {
         //REGISTER USER ON BUTTON CLICK
         signUpButton.setOnClickListener {
             val username = usernameField.text.toString()
-            val password = Utils.hashString("SHA-512", passwordField.text.toString())
+            val password = Utils.sha512(passwordField.text.toString())
 
-            launch {
-                try {
-                    val userExists = Utils.checkUserExists(username).readText().toBoolean()
-                    if (!userExists) {
-                        signup(username, password)
-                        Toast.makeText(applicationContext, "Registration completed successfully !", Toast.LENGTH_LONG)
-                            .show()
-                    } else {
-                        Toast.makeText(applicationContext, "Username already exists", Toast.LENGTH_LONG)
-                            .show()
+            //CHECK FIELD IS NOT EMPTY
+            if (username.isEmpty() or (password.length < 8)) {
+                Toast.makeText(applicationContext, "Please enter a valid username and password", Toast.LENGTH_LONG)
+                    .show()
+            } else {
+                launch {
+                    try {
+                        //SIGNUP IF USER DOES NOT EXIST
+                        val userExists = Utils.sendRequest("checkuser", username, password).readText().toBoolean()
+                        if (!userExists) {
+                            Utils.sendRequest("signup", username, password)
+                            Toast.makeText(
+                                applicationContext,
+                                "Registration completed successfully !",
+                                Toast.LENGTH_LONG
+                            )
+                                .show()
+                        } else {
+                            Toast.makeText(applicationContext, "Username already exists", Toast.LENGTH_LONG)
+                                .show()
+                        }
+
+                    } catch (e: Exception) {
+                        Toast.makeText(
+                            applicationContext,
+                            "Oops! Something went wrong, please try again later",
+                            Toast.LENGTH_LONG
+                        ).show()
                     }
-
-                } catch (e: Exception) {
-                    Toast.makeText(
-                        applicationContext,
-                        "Oops! Something went wrong, please try again later",
-                        Toast.LENGTH_LONG
-                    ).show()
                 }
             }
-        }
-    }
-
-    //SIGNUP METHOD
-    private suspend fun signup(username: String, password: String): HttpResponse {
-        val httpClient = HttpClient(Android) {
-            engine {
-                connectTimeout = 2000
-                socketTimeout = 2000
-            }
-        }
-
-        return httpClient.post("http://192.168.0.7:8000/signup") {
-            val userinfo = Utils.UserInfo(username = username, password = password)
-            val postData = gson.toJson(userinfo)
-            body = postData
         }
     }
 }
