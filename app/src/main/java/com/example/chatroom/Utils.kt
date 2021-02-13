7f7c4bd7e26589582f33ebcc809cf67df2e53022
Rushad Mehta <rushadmehta16@gmail.com>
2021-02-13 15:41:50 +0530

    Moved UserInfo data class from MainActivity.kt to Utils object. Added hashString method in Utils for SHA-512 encoding.

--- app/src/main/java/com/example/chatroom/Utils.kt
+++ app/src/main/java/com/example/chatroom/Utils.kt
@@ -1,14 +1,21 @@
 package com.example.chatroom
 
 import com.google.gson.Gson
+import com.google.gson.annotations.SerializedName
 import io.ktor.client.*
 import io.ktor.client.engine.android.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
+import java.security.MessageDigest
 
 object Utils {
 
     private val gson = Gson()
+
+    data class UserInfo(
+        @SerializedName("username") val username: String? = null,
+        @SerializedName("password") val password: String? = null
+    )
 
     suspend fun checkUserExists(username: String): HttpResponse {
         val httpClient = HttpClient(Android) {
@@ -24,4 +31,26 @@ object Utils {
             body = postData
         }
     }
+
+    fun hashString(type: String, input: String): String {
+        val HEX_CHARS = "0123456789ABCDEF"
+        val bytes = MessageDigest
+            .getInstance(type)
+            .digest(input.toByteArray())
+        val result = StringBuilder(bytes.size * 2)
+
+        bytes.forEach {
+            val i = it.toInt()
+            result.append(HEX_CHARS[i shr 4 and 0x0f])
+            result.append(HEX_CHARS[i and 0x0f])
+        }
+
+        return result.toString()
+    }
+
+    data class Person (
+        val fnmae: String,
+        val lname: String
+            )
+
 }
