48853b04d38bec7945f4e2cf0b7f14d390ead3f1
Rushad Mehta <rushadmehta16@gmail.com>
2021-02-13 17:33:45 +0530

    Removed individual request methods, and created a consolidated sendRequest method. Modified hashString method and renamed it to sha512. Removed redundant methods.

--- app/src/main/java/com/example/chatroom/Utils.kt
+++ app/src/main/java/com/example/chatroom/Utils.kt
@@ -12,12 +12,31 @@ object Utils {
 
     private val gson = Gson()
 
+    //USER CREDENTIALS CLASS
     data class UserInfo(
-        @SerializedName("username") val username: String? = null,
-        @SerializedName("password") val password: String? = null
+        @SerializedName("username") val username: String,
+        @SerializedName("password") val password: String
     )
 
-    suspend fun checkUserExists(username: String): HttpResponse {
+    //SHA-512 ENCODER METHOD
+    fun sha512(input: String): String {
+        val chars = "0123456789ABCDEF"
+        val bytes = MessageDigest
+            .getInstance("SHA-512")
+            .digest(input.toByteArray())
+        val result = StringBuilder(bytes.size * 2)
+
+        bytes.forEach {
+            val i = it.toInt()
+            result.append(chars[i shr 4 and 0x0f])
+            result.append(chars[i and 0x0f])
+        }
+
+        return result.toString()
+    }
+
+    //COMMON REQUEST METHOD
+    suspend fun sendRequest(requestAction: String, username: String, password: String): HttpResponse {
         val httpClient = HttpClient(Android) {
             engine {
                 connectTimeout = 2000
@@ -25,26 +44,12 @@ object Utils {
             }
         }
 
-        return httpClient.post("http://192.168.0.7:8000/checkuser") {
-            val userinfo = UserInfo(username = username)
+        val url = "http://192.168.0.7:8000/$requestAction"
+
+        return httpClient.post(url) {
+            val userinfo = UserInfo(username = username, password = password)
             val postData = gson.toJson(userinfo)
             body = postData
         }
-    }
-
-    fun hashString(type: String, input: String): String {
-        val HEX_CHARS = "0123456789ABCDEF"
-        val bytes = MessageDigest
-            .getInstance(type)
-            .digest(input.toByteArray())
-        val result = StringBuilder(bytes.size * 2)
-
-        bytes.forEach {
-            val i = it.toInt()
-            result.append(HEX_CHARS[i shr 4 and 0x0f])
-            result.append(HEX_CHARS[i and 0x0f])
-        }
-
-        return result.toString()
     }
 }
