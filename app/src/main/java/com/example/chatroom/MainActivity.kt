6a9c321467dad8940ab64184ec02fd9ce5b875f4
Rushad Mehta <rushadmehta16@gmail.com>
2021-02-13 15:37:12 +0530

    Restructured file, moved UserInfo data class to Utils.kt

--- app/src/main/java/com/example/chatroom/MainActivity.kt
+++ app/src/main/java/com/example/chatroom/MainActivity.kt
@@ -5,13 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.gson.annotations.SerializedName
 
-
-data class UserInfo(
-    @SerializedName("username") val username: String? = null,
-    @SerializedName("password") val password: String? = null
-)
 
 class MainActivity : AppCompatActivity() {
 
