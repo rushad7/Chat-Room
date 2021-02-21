de18d777a61a5afcf5ffd3b22fd0111e2ff5b378
Rushad Mehta <rushadmehta16@gmail.com>
2021-02-21 18:02:57 +0530

    Check of saved login preference when app opens.

--- app/src/main/java/com/example/chatroom/MainActivity.kt
+++ app/src/main/java/com/example/chatroom/MainActivity.kt
@@ -5,25 +5,52 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
+
+    //START COROUTINE
+    private var job: Job = Job()
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val loginSettings = LoginSettings(this)
+        val isLoggedIn = loginSettings.getState()
+        val chatIntent = Intent(this, ChatPage::class.java)
 
-        val signUpButton = findViewById<TextView>(R.id.signup)
-        val logInButton = findViewById<Button>(R.id.login)
+        launch {
+            if (isLoggedIn) {
+                val userCredentials = loginSettings.getUserInfo()
+                val username = userCredentials.first.toString()
+                val password = userCredentials.second.toString()
 
-        signUpButton.setOnClickListener {
-            val signUpIntent = Intent(this, SignUp::class.java)
-            startActivity(signUpIntent)
-        }
-        logInButton.setOnClickListener {
-            val logInIntent = Intent(this, LogIn::class.java)
-            startActivity(logInIntent)
+                Utils.sendRequest("login", username, password)
+                startActivity(chatIntent)
+            } else {
+
+                super.onCreate(savedInstanceState)
+                setContentView(R.layout.activity_main)
+
+                val signUpButton = findViewById<TextView>(R.id.signup)
+                val logInButton = findViewById<Button>(R.id.login)
+
+                signUpButton.setOnClickListener {
+                    val signUpIntent = Intent(this@MainActivity, SignUp::class.java)
+                    startActivity(signUpIntent)
+                }
+                logInButton.setOnClickListener {
+                    val logInIntent = Intent(this@MainActivity, LogIn::class.java)
+                    startActivity(logInIntent)
+                }
+            }
         }
     }
 }
