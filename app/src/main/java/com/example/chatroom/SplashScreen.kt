2bba697e204be86a550874114bb5154153003f11
Rushad Mehta <rushadmehta16@gmail.com>
2021-02-27 22:41:27 +0530

    Added Splash Screen Activity

---
+++ app/src/main/java/com/example/chatroom/SplashScreen.kt
@@ -0,0 +1,22 @@
+package com.example.chatroom
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashScreen: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val mainActivityIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainActivityIntent)
+            finish()
+        }, 500)
+    }
+}
