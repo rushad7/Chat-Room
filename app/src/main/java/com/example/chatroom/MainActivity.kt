8896336cae3e8cee200c8cbfcf47e889dd07bf12
Rushad Mehta <rushadmehta16@gmail.com>
2021-02-27 22:45:59 +0530

    Restructured project so that SplashScreen.kt is the first Activity. Removed redundant coroutines. Improved exception handling for Network IO.

--- app/src/main/java/com/example/chatroom/MainActivity.kt
+++ app/src/main/java/com/example/chatroom/MainActivity.kt
@@ -4,58 +4,46 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
-import kotlin.coroutines.CoroutineContext
 
 
-class MainActivity : AppCompatActivity(), CoroutineScope {
-
-    //START COROUTINE
-    private var job: Job = Job()
-    override val coroutineContext: CoroutineContext
-        get() = Dispatchers.Main + job
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        val loginSettings = LoginSettings(this)
+
+        val loginSettings = LoginSettings(this@MainActivity)
         val isLoggedIn = loginSettings.getState()
         val chatIntent = Intent(this, ChatPage::class.java)
 
-        launch {
             try {
                 if (isLoggedIn) {
                     val userCredentials = loginSettings.getUserInfo()
-                    val username = userCredentials.first.toString()
-                    val password = userCredentials.second.toString()
+                    val username = userCredentials.first
+                    val password = userCredentials.second
 
-                    Utils.sendRequest("login", username, password)
-                    startActivity(chatIntent)
+                        if (username != null && password != null) {
+                            startActivity(chatIntent)
+                        }
+
                 } else {
-
-                    super.onCreate(savedInstanceState)
                     setContentView(R.layout.activity_main)
 
                     val signUpButton = findViewById<TextView>(R.id.signup)
                     val logInButton = findViewById<Button>(R.id.login)
 
                     signUpButton.setOnClickListener {
-                        val signUpIntent = Intent(this@MainActivity, SignUp::class.java)
+                        val signUpIntent = Intent(this, SignUp::class.java)
                         startActivity(signUpIntent)
                     }
                     logInButton.setOnClickListener {
-                        val logInIntent = Intent(this@MainActivity, LogIn::class.java)
+                        val logInIntent = Intent(this, LogIn::class.java)
                         startActivity(logInIntent)
                     }
                 }
             } catch (e: Exception) {
-                Toast.makeText(this@MainActivity, "Oops! Something went wrong.", Toast.LENGTH_LONG).show()
-            }
+                setContentView(R.layout.network_error)
         }
     }
 }
